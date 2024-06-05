735e3b9147d56b4d0725136cfc3c8618426af520
Rommel Vieira Carneiro <rommelcarneiro@gmail.com>
2024-06-05 18:21:57 -0300

    Inicialização do JSONServer a partir do index.js

--- index.js
+++ index.js
@@ -16,13 +16,11 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('./db/db.json')
-const cors = require('cors');
 
 // Para permitir que os dados sejam alterados, altere a linha abaixo
 // colocando o atributo readOnly como false.
 const middlewares = jsonServer.defaults()
 
-server.use(cors())
 server.use(middlewares)
 server.use(router)
 server.listen(3000, () => {
